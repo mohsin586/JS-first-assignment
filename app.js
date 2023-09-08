f8137d62d555b168e1ccf22ee9105e23fe812703
mohsin586 <143820469+mohsin586@users.noreply.github.com>
2023-09-08 12:37:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,143 @@
+//Chapter 01:
+//01
+alert("Mohsin ")
+alert("Ali")
+alert("mohsinali@hotmail.com")
+alert("03468487586")
+alert("**********")
+
+//02
+alert("You'r learning javaScript")
+
+//03
+alert("keep calm, just codding")
+
+
+
+//---------------------------------------------------
+
+//Chapter 02:
+//01
+var firstName;
+
+
+//02
+var course;
+course="wed development";
+
+
+//03
+var teamName="Rainbow friends";
+alert(teamName)
+
+//04
+var bestMan="Charlie";
+var statment=". This statement has already been coded.";
+
+
+//---------------------------------------------------
+//Chapter 03:
+//01
+var caseQty;
+
+
+
+//02
+caseQty=144;
+
+
+//03
+var add= caseQty + 6;
+console.log(add);
+
+//04
+var sum;
+sum= 60 +40;
+console.log(sum);
+
+//05
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+//06
+var num1=23;
+num1=num1+43;
+console.log(num1)
+
+
+//---------------------------------------------------
+//Chapter 04:
+//01
+var productCost=3.45;
+
+//02
+var nameOfBand;
+
+//03
+var product=234;
+
+//04
+var userName="Mohsin Ali";
+
+//05
+//legal Variables:
+var firstName="ali";
+var add, sub, divide;
+var _num1 = 15;
+var $num2 = 20;
+var __num3 = 30;
+var $$num4 = 40;
+  
+// Print the result
+console.log(firstName);
+console.log(_num1);
+console.log($num2);
+console.log(__num3);
+console.log($$num4);
+
+//illegal Variables:
+// var 12;
+// var num-1;
+// var user name="ali";
+// var @=34;
+
+
+//---------------------------------------------------
+//Chapter 05:
+//01
+// name and symbol 
+// modulo operator % ;
+
+
+//02
+var num = 20 % 6;
+console.log(num);
+//value=2
+
+//03
+var largeNum=1000;
+console.log(largeNum*2000);
+
+
+//04
+var numb;
+var numb2=66;
+numb=numb2 - 36;
+console.log(numb);
+
+
+//05
+var remaindr;
+remaindr= 234 % 10;
+console.log(remaindr);
+
+//06
+var number1=10 ,number2=12;
+var ans= number1*number2;
+alert("product of" + number1 + "and " + number2 + "is"+ ans);
+
+
+
+//---------------------------------------------------
